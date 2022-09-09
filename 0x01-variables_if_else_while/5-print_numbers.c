f79eb6d5d7ae789cc9ef2fff13a7f78a4b41adf3
Esayas188 <esayasfekade188@gmail.com>
2022-09-09 16:12:18 -0700

    shsdudj

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints number 0 to 9
+ *
+ * Return: Always (success)
+ */
+int main (void)
+{
+	char n;
+
+	for(n = '0'; n <= '9'; n++)
+		putchar(n);
+
+	putchar('\n');
+	return (0);
+}
