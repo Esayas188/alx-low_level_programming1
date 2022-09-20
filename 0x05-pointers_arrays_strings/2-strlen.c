be29e6cf5fda50b404e5c6e6e664b68085cd7c74
Esayas188 <esayasfekade188@gmail.com>
2022-09-19 18:23:00 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen -> function to get the lengeth of a string
+ * @s: string pointer to passed to this function
+ * Return: returns lengeth of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *, int *b);
+int _strlen(char *s);
 
 #endif
